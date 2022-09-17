42d44df67a63a67332c74669a9a5dcc12e41860d
Marllef H. A. Freitas <marllef-alves@hotmail.com>
2022-09-16 22:19:11 -0300

    Ajustando projeto

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  root: '.',
+  build: {
+    outDir: '../server/dist/client',
+  },
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    },
+  },
+});
